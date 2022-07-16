9b60fdd7f671d9969b91336d347d5ff5a20a703f
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2022-07-16 23:11:47 +0530

    Added unit test cases

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WeatherService } from './weather.service';
 
-describe('WeatherService', () => {
+fdescribe('WeatherService', () => {
   let service: WeatherService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        WeatherService
+      ]
+    });
     service = TestBed.inject(WeatherService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('test getWeatherData', () => {
+    const validCityName = 'Sydney';
+    expect(service.getWeatherData(validCityName)).toBeTruthy();
+
+    const emptyCityName = '';
+    expect(service.getWeatherData(emptyCityName)).toBeTruthy();
+  });
+
 });
